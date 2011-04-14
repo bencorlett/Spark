00dd78d6acfff6bcf65377bef8428cf7b1a196b1
Ben Corlett <bencorlett@me.com>
2011-04-14 23:01:07 +1000

    Added Fuel::register($key, $value) and Fuel::registry($key).
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- classes/fuel.php
+++ classes/fuel.php
@@ -21,4 +21,74 @@ namespace Spark;
 
 class Fuel extends \Fuel\Core\Fuel {
 	
+	/**
+	 * Items in the registry
+	 * 
+	 * @var	array
+	 */
+	protected static $_registry = array();
+	
+	/**
+	 * Register
+	 * 
+	 * Register something in the registry.
+	 * 
+	 * If the first parameter is an
+	 * associative array all data in the
+	 * array will be added to the registry
+	 * 
+	 * If the data is a string, the value for
+	 * the second parameter will be set as the
+	 * value for that data
+	 * 
+	 * @access	public
+	 * @param	string	Key
+	 * @param	mixed	Value
+	 */
+	public static function register($key, $value = null)
+	{
+		if (is_array($key))
+		{
+			foreach ($key as $key => $value)
+			{
+				self::$_registry[$key] = $value;
+			}
+			
+			return;
+		}
+		
+		self::$_registry[$key] = $value;
+	}
+	
+	/**
+	 * Registry
+	 * 
+	 * Get something out of the registry
+	 * 
+	 * If the first parameter is empty, the
+	 * entire registry will be returned
+	 * 
+	 * If the first parameter is provided
+	 * and the item doesnt' exist, whatever
+	 * is provided as the default in the second
+	 * parameter will be returned
+	 * 
+	 * @access	public
+	 * @param	string	Key
+	 * @param	mixed	Default
+	 */
+	public static function registry($key = null, $default = null)
+	{
+		if (is_null($key))
+		{
+			return self::$_registry;
+		}
+		
+		if ( ! isset(self::$_registry[$key]))
+		{
+			return $default;
+		}
+		
+		return self::$_registry[$key];
+	}
 }
