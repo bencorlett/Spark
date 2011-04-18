0270684966dcf0800875fdb7dde5332b808dbde0
Ben Corlett <bencorlett@me.com>
2011-04-18 22:08:16 +1000

    Started Breadcrumbs class.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- classes/breadcrumbs.php
+++ classes/breadcrumbs.php
@@ -21,4 +21,115 @@ namespace Spark;
 
 class Breadcrumbs extends \Object {
 	
+	/**
+	 * Add
+	 *
+	 * Adds a new breadcrumb to
+	 * the class instance
+	 * 
+	 * If you have previously added
+	 * a level 3 breadcrumb and add a
+	 * level 2 breadcrumb, it will
+	 * remove any level 3 or greater
+	 * breadcrumbs
+	 * 
+	 * @access	public
+	 * @param	int		Level
+	 * @param	string	Text
+	 * @param	string	Uri
+	 */
+	public static function add($level, $text, $uri = null)
+	{
+		// We need a level
+		if ( ! is_int($level))
+		{
+			throw new Exception('The level provided \'%s\' isn\'nt an integer', $level);
+		}
+		
+		// Add the breadcrumb
+		static::instance()->_add($level, $text, $uri);
+	}
+	
+	/**
+	 * Add
+	 * 
+	 * Adds a new breadcrumb to
+	 * the session
+	 * 
+	 * If you have previously added
+	 * a level 3 breadcrumb and add a
+	 * level 2 breadcrumb, it will
+	 * remove any level 3 or greater
+	 * breadcrumbs
+	 * 
+	 * @access	public
+	 * @param	int		Level
+	 * @param	string	Text
+	 * @param	string	Uri
+	 */
+	protected function _add($level, $text, $uri = null)
+	{
+		// \Session::delete('breadcrumbs');
+		// Get or initialise breadcrumbs
+		if (($old_breadcrumbs = \Session::get('breadcrumbs')) === null)
+		{
+			$old_breadcrumbs = \Object::factory();
+		}
+		
+		$new_breadcrumbs = \Object::factory();
+		
+		// Loop through old breadcrumbs
+		// and add all breadcrumbs to the new
+		// breadcrumbs only if the level
+		// is less than the new level
+		foreach ($old_breadcrumbs->get_data() as $breadcrumb_level => $breadcrumb)
+		{
+			if ($breadcrumb_level >= $level)
+			{
+				continue;
+			}
+			
+			// Add data
+			$new_breadcrumbs->add_data(array(
+				$breadcrumb_level => $breadcrumb
+			));
+		}
+		
+		// Set the data
+		$new_breadcrumbs->add_data(array($level => array('text' => $text, 'uri' => $uri)));
+		
+		// Set the breadcrumbs
+		\Session::set('breadcrumbs', $new_breadcrumbs);
+	}
+	
+	/**
+	 * Render
+	 * 
+	 * Renders the Breadcrumbs
+	 * object as Html
+	 * 
+	 * @access	public
+	 * @return	string	Html
+	 */
+	public static function render()
+	{
+		return static::instance()->_render();
+	}
+	
+	/**
+	 * Render
+	 * 
+	 * Renders the Breadcrumbs
+	 * object as Html
+	 * 
+	 * @access	protected
+	 * @return	string	Html
+	 */
+	protected function _render()
+	{
+		$view = \View::factory('breadcrumbs/default')
+					 ->set('breadcrumbs', $this->get_breadcrumbs());
+		
+		return $view;
+	}
 }
