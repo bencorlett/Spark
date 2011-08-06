3ca7e514102330ce3bbdd9748fb4a66546fbf15a
Ben Corlett <bencorlett@me.com>
2011-08-06 17:30:45 +1000

    Starting fresh.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- bootstrap.php
+++ bootstrap.php
@@ -21,58 +21,5 @@ Autoloader::add_core_namespace('Spark');
 
 Autoloader::add_classes(array(
 	
-	// Agent
-	// Until everyone's made up their
-	// mind on the agent class I'm
-	// putting it here as it's possibly one
-	// of the most useful classes out
-	'Fuel\\Core\\Agent'									=> COREPATH . '/classes/agent.php',
-	'Spark\\Agent'										=> __DIR__ . '/classes/agent.php',
-	
-	// BackFace
-	'Spark\\BackFace'									=> __DIR__ . '/classes/backface.php',
-	
-	// Breadcrumbs
-	'Spark\\Breadcrumbs'								=> __DIR__ . '/classes/breadcrumbs.php',
-	
-	// Cron
-	'Spark\\Cron'										=> __DIR__ . '/classes/cron.php',
-	
-	// Date
-	'Spark\\Date'										=> __DIR__ . '/classes/date.php',
-	
-	// Exception
-	'Spark\\Exception'									=> __DIR__ . '/classes/exception.php',
-	
-	// Grid
 	'Spark\\Grid'										=> __DIR__ . '/classes/grid.php',
-	'Spark\\Grid_Column'								=> __DIR__ . '/classes/grid/column.php',
-	'Spark\\Grid_Column_Filter_Abstract'				=> __DIR__ . '/classes/grid/column/filter/abstract.php',
-	'Spark\\Grid_Column_Filter_Date'					=> __DIR__ . '/classes/grid/column/filter/date.php',
-	'Spark\\Grid_Column_Filter_Text'					=> __DIR__ . '/classes/grid/column/filter/text.php',
-	'Spark\\Grid_Column_Renderer_Abstract'				=> __DIR__ . '/classes/grid/column/renderer/abstract.php',
-	'Spark\\Grid_Column_Renderer_Date'					=> __DIR__ . '/classes/grid/column/renderer/date.php',
-	'Spark\\Grid_Column_Renderer_Text'					=> __DIR__ . '/classes/grid/column/renderer/text.php',
-	'Spark\\Grid_Driver_Abstract'						=> __DIR__ . '/classes/grid/driver/abstract.php',
-	'Spark\\Grid_Driver_KohanaOrm'						=> __DIR__ . '/classes/grid/driver/kohanaorm.php',
-	'Spark\\Grid_Driver_Orm'							=> __DIR__ . '/classes/grid/driver/orm.php',
-	'Spark\\Grid_Massaction'							=> __DIR__ . '/classes/grid/massaction.php',
-	
-	// Notifications
-	'Spark\\Notifications'								=> __DIR__ . '/classes/notifications.php',
-	
-	// Object
-	'Spark\\Object'										=> __DIR__ . '/classes/object.php',
-	
-	// Spark
-	'Spark\\Spark'										=> __DIR__ . '/classes/spark.php',
-	
-	// Str
-	'Spark\\Str'										=> __DIR__ . '/classes/str.php',
-	
-	// Tabs
-	'Spark\\Tabs'										=> __DIR__ . '/classes/tabs.php',
-	
-	// View
-	'Spark\\View'										=> __DIR__ . '/classes/view.php',
 ));
